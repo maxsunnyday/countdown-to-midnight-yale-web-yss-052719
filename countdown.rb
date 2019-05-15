15ae97901006dd611aeb42d11ef7becb13fa9486
Max Sun <max.sun@yale.edu>
2019-05-15 05:49:44 +0000

    Midnight

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
 end
