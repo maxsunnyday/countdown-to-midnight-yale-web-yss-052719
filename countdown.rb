105b0da9a9f3b7bfb87a23a688bc9b98dfa40305
Max Sun <max.sun@yale.edu>
2019-05-15 05:40:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
 def countdown(time)
-  while time >= 0
+  while time > 0
     puts "#{time} SECOND(S)!"
     time -= 1
   end
-  puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
