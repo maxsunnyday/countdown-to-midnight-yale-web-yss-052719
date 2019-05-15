b45325e73050fc08ac9542310c1116e1edf4830d
Max Sun <max.sun@yale.edu>
2019-05-15 05:38:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(time)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    sleep 1
+  end
+  puts "HAPPY NEW YEAR!"  
+end
